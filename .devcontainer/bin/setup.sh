4881d089758116c660d3108ef4a6a15b5d08c5ec
Blake Green <blakegreen@msn.com>
2020-11-18 16:52:42 +0000

    changing

--- .devcontainer/bin/setup.sh
+++ .devcontainer/bin/setup.sh
@@ -10,7 +10,7 @@ is_ec2() {
 
 # if in codespaces
 if [[ $CODESPACES == 'true' ]]; then
-  printf 'ZSH_THEME="clean"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
+  printf 'ZSH_THEME="spaceship"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
   # change 'docker' group to gid 800 
   sudo groupmod -g 800 docker
   # add current user to `docker` group
@@ -25,7 +25,7 @@ else
     # add current user to `root` group
     sudo usermod -a -G root $USERNAME
   fi
-#   printf 'ZSH_THEME="powerlevel9k/powerlevel9k"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nPOWERLEVEL9K_MODE="nerdfont-complete"\nPOWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable)\nPOWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status vcs virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
+  printf 'ZSH_THEME="spaceship"\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
 fi
 
 
