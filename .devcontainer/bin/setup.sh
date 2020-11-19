fe5a95260ca39a2dda7e4a45af85743314aa0785
Blake Green <blakegreen@msn.com>
2020-11-18 21:24:12 -0800

    Update setup.sh

--- .devcontainer/bin/setup.sh
+++ .devcontainer/bin/setup.sh
@@ -2,10 +2,8 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/dev/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 brew install helm	
 brew install pipenv	
