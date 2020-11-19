dc5ac5732bb4bea1dc384d0004367bb34411f8c8
Blake Green <blakegreen@msn.com>
2020-11-19 13:57:38 -0800

    Update setup.sh

--- .devcontainer/bin/setup.sh
+++ .devcontainer/bin/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/$USERNAME/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv); exit 0
+
+brew install helm	
+brew install pipenv	
+brew install pulumi 
+brew install terraform 	
+brew install cdk8s 	
+brew install ruby
